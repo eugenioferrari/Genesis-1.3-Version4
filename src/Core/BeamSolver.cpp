20ddd8ad3d29cf2d028ec345b1c5244b47329a39
eugenioferrari <97533647+eugenioferrari@users.noreply.github.com>
2024-05-08 11:53:34 +0200

    Different approach to find the slicing parameters

--- src/Core/BeamSolver.cpp
+++ src/Core/BeamSolver.cpp
@@ -52,8 +52,8 @@ void BeamSolver::advance(double delz, Beam *beam, vector< Field *> *field, Undul
         efield.shortRange(&beam->beam.at(is), beam->current.at(is), gammaz2, is);
         // Calculate the LSC due to HGHG
         cout << "ISLICE=" << is << endl;
-        cout << "hghg space charge with current=" << beam->current.at(is) << "A. Slicelength = " << field->at(is)->slicelength << " slicespacing="<< field->at(is)->xlambda << "and LDRIFT=" << delz << endl;
-        efield.hghgRange(&beam->beam.at(is), beam->current.at(is), field->at(is)->slicelength, field->at(is)->xlambda, delz);
+        cout << "hghg space charge with current=" << beam->current.at(is) << "A. Slicelength = " << beam->slicelength << " slicespacing="<< beam->reflength << "and LDRIFT=" << delz << endl;
+        efield.hghgRange(&beam->beam.at(is), beam->current.at(is), beam->slicelength, beam->reflength, delz);
         cout << "npart " << beam->beam.at(is).size() << endl;
         for (int ip = 0; ip < beam->beam.at(is).size(); ip++) {
             gamma = beam->beam.at(is).at(ip).gamma;
